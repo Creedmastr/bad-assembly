ca593e9210bd0c02af1b9c2f0cc74b1f8614f9a0
Creedmastr <thegaminglife09@gmail.com>
2023-10-19 20:34:42 +0200

    refactored a bit

--- src/registers/reg_struct.rs
+++ src/registers/reg_struct.rs
@@ -4,20 +4,8 @@ pub struct Register<T> {
     pub value: T
 }
 
-impl Register<i32> {
-    pub fn update_value(&self, x: i32) -> Self {
-        let new = Register {
-            name: self.name.clone(),
-
-            value: x
-        };
-
-        return new;
-    }
-}
-
-impl Register<String> {
-    pub fn update_value(&self, x: String) -> Self {
+impl<T> Register<T> {
+    pub fn update_value(&self, x: T) -> Self {
         let new = Register {
             name: self.name.clone(),
 
